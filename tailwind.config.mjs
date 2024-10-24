91720b80d7a3c4de77164571ff32a05e974712e4
Joaquin <joaco.alonso.garcia@gmail.com>
2024-10-24 11:07:04 -0500

    checkpoint challenge finished

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,16 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				'primary': ['Outfit', 'sans-serif'],
+			},
+			colors: {
+				'slate-300': 'hsl(212, 45%, 89%)',
+				'slate-500': 'hsl(216, 15%, 48%)',
+				'slate-900': 'hsl(218, 44%, 22%)',
+			},
+		},
 	},
 	plugins: [],
 }
